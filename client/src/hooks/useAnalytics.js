8ff41c6d64362b7684b7008449500b2a64cb9a40
Tonyjr7 <anthonyayokunle777@gmail.com>
2025-09-26 20:17:52 +0100

    fix:  google analytics

--- client/src/hooks/useAnalytics.js
+++ client/src/hooks/useAnalytics.js
@@ -2,7 +2,7 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
-const GA_ID = 'G-PR9T65FV9W';
+const GA_ID = 'G-G94VYLGJM0';
 
 export default function useAnalytics() {
   const location = useLocation();
